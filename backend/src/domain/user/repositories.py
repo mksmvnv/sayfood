5e7422024ad69a4960748bfa5009ac36079f3367
Maksim <mksmvnv@icloud.com>
2026-05-29 16:52:07 +0300

    feat: add user repository interface

---
+++ backend/src/domain/user/repositories.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from uuid import UUID
+
+from src.domain.user.aggregates import User
+from src.domain.user.value_objects import Email
+
+
+@dataclass
+class UserRepository(ABC):
+    """Abstract user repository."""
+
+    @abstractmethod
+    async def add(self, user: User) -> None:
+        """Add new user."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_id(self, id: UUID) -> User | None:
+        """Get user by ID."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update(self, user: User) -> None:
+        """Update user."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_email(self, email: Email) -> User | None:
+        """Get user by email."""
+        raise NotImplementedError
