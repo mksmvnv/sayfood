55d79bf7e460916748d371576dc7cd4a2cbd2446
Maksim <mksmvnv@icloud.com>
2026-06-01 23:18:54 +0300

    chore: add user not found exception

--- backend/src/domain/user/exceptions.py
+++ backend/src/domain/user/exceptions.py
@@ -8,7 +8,7 @@ class InvalidEmail(DomainException):
     """Raised when email is invalid."""
 
     code: str = "invalid_email"
-    detail: str = "Invalid Email."
+    detail: str = "Invalid Email"
 
 
 @dataclass(eq=False, frozen=True)
@@ -16,4 +16,12 @@ class InvalidHashedPassword(DomainException):
     """Raised when hashed password is invalid."""
 
     code: str = "invalid_hashed_password"
-    detail: str = "Invalid Hashed Password."
+    detail: str = "Invalid Hashed Password"
+
+
+@dataclass(eq=False, frozen=True)
+class UserNotFound(DomainException):
+    """User not found exception."""
+
+    code: str = "user_not_found"
+    detail: str = "User not found"
