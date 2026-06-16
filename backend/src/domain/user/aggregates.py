11a01336ddc24478d1a265e9090ac7b4d59f3fea
Maksim <mksmvnv@icloud.com>
2026-06-16 23:33:54 +0300

    feat: add sessions and exceptions

--- backend/src/domain/user/aggregates.py
+++ backend/src/domain/user/aggregates.py
@@ -1,31 +1,49 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from datetime import datetime
 from typing import Self
 
 from src.domain.seedwork.aggregates import AggregateRoot
+from src.domain.user.entities import SessionEntity
 from src.domain.user.events import (
+    UserAllSessionsRevoked,
     UserCreated,
     UserDeleted,
     UserEmailChanged,
     UserIsActiveChanged,
     UserIsAdminChanged,
     UserPasswordChanged,
+    UserSessionCreated,
+    UserSessionRevoked,
+)
+from src.domain.user.exceptions import (
+    EmailAlreadySame,
+    SessionAlreadyExists,
+    SessionNotFound,
+    UserAlreadyActivated,
+    UserAlreadyDeactivated,
+    UserAlreadyDemoted,
+    UserAlreadyPromoted,
 )
 from src.domain.user.value_objects import Email, HashedPassword
 
 
 @dataclass(eq=False, kw_only=True)
-class User(AggregateRoot):
+class UserAggregate(AggregateRoot):
     """User aggregate."""
 
     email: Email
     hashed_password: HashedPassword
     is_active: bool = True
     is_admin: bool = False
+    _sessions: list[SessionEntity] = field(default_factory=list, init=False)
 
     @classmethod
     def create(cls, email: Email, hashed_password: HashedPassword) -> Self:
         """Create user."""
-        user = cls(email=email, hashed_password=hashed_password)
+        user = cls(
+            email=email,
+            hashed_password=hashed_password,
+        )
 
         user.register_event(
             UserCreated(
@@ -36,10 +54,15 @@ class User(AggregateRoot):
 
         return user
 
+    @property
+    def sessions(self) -> list[SessionEntity]:
+        """Return a copy of user sessions."""
+        return self._sessions.copy()
+
     def activate(self) -> None:
         """Activate user."""
         if self.is_active:
-            return None
+            raise UserAlreadyActivated()
         self.is_active = True
         self.register_event(
             UserIsActiveChanged(
@@ -52,7 +75,7 @@ class User(AggregateRoot):
     def deactivate(self) -> None:
         """Deactivate user."""
         if not self.is_active:
-            return None
+            raise UserAlreadyDeactivated()
         self.is_active = False
         self.register_event(
             UserIsActiveChanged(
@@ -65,7 +88,7 @@ class User(AggregateRoot):
     def promote_to_admin(self) -> None:
         """Promote user to admin."""
         if self.is_admin:
-            return None
+            raise UserAlreadyPromoted()
         self.is_admin = True
         self.register_event(
             UserIsAdminChanged(
@@ -78,7 +101,7 @@ class User(AggregateRoot):
     def demote_from_admin(self) -> None:
         """Demote user from admin."""
         if not self.is_admin:
-            return None
+            raise UserAlreadyDemoted()
         self.is_admin = False
         self.register_event(
             UserIsAdminChanged(
@@ -100,6 +123,8 @@ class User(AggregateRoot):
 
     def change_email(self, email: Email) -> None:
         """Change user email."""
+        if self.email == email:
+            raise EmailAlreadySame()
         self.email = email
         self.register_event(
             UserEmailChanged(
@@ -117,3 +142,44 @@ class User(AggregateRoot):
                 email=self.email,
             )
         )
+
+    def add_session(self, token: str, expires_at: datetime) -> None:
+        """Add user session."""
+        if any(session.token == token for session in self._sessions):
+            raise SessionAlreadyExists()
+        self._sessions.append(
+            SessionEntity(
+                token=token,
+                expires_at=expires_at,
+            )
+        )
+        self.register_event(
+            UserSessionCreated(
+                user_id=self.id,
+            )
+        )
+        self._touch()
+
+    def revoke_session(self, token: str) -> None:
+        """Revoke a specific user session."""
+        for session in self._sessions:
+            if session.token == token:
+                self._sessions.remove(session)
+                self.register_event(
+                    UserSessionRevoked(
+                        user_id=self.id,
+                    )
+                )
+                self._touch()
+                return
+        raise SessionNotFound
+
+    def revoke_all_sessions(self) -> None:
+        """Revoke all user sessions."""
+        self._sessions.clear()
+        self.register_event(
+            UserAllSessionsRevoked(
+                user_id=self.id,
+            )
+        )
+        self._touch()
