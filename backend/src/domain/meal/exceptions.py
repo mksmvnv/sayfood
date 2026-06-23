e13ca589121097c30eab7fa010c4c9144d90e44e
Maksim <mksmvnv@icloud.com>
2026-06-23 22:57:19 +0300

    feat: add meal domain exceptions

---
+++ backend/src/domain/meal/exceptions.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+
+from src.domain.seedwork.exceptions import DomainException
+
+
+@dataclass(eq=False)
+class InvalidGoal(DomainException):
+    """Raised when goal is invalid."""
+
+    code: str = "invalid_goal"
+    detail: str = "Invalid goal"
+
+
+@dataclass(eq=False)
+class InvalidWeight(DomainException):
+    """Raised when weight is invalid."""
+
+    code: str = "invalid_weight"
+    detail: str = "Invalid weight"
+
+
+@dataclass(eq=False)
+class InvalidHeight(DomainException):
+    """Raised when height is invalid."""
+
+    code: str = "invalid_height"
+    detail: str = "Invalid height"
+
+
+@dataclass(eq=False)
+class InvalidAge(DomainException):
+    """Raised when age is invalid."""
+
+    code: str = "invalid_age"
+    detail: str = "Invalid age"
+
+
+@dataclass(eq=False)
+class InvalidActivityLevel(DomainException):
+    """Raised when activity level is invalid."""
+
+    code: str = "invalid_activity_level"
+    detail: str = "Invalid activity level"
